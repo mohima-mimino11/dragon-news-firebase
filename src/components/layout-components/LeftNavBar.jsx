398e2b1b6bf095365c692db3522a8bbb502a8802
mohima-mimino11 <mohimahoque22@gmail.com>
2025-04-11 20:39:29 +0600

    leftnavbar layout design added

--- src/components/layout-components/LeftNavBar.jsx
+++ src/components/layout-components/LeftNavBar.jsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import kid from "../../assets/kid.png";
+import office from "../../assets/office.png";
+import stadium from "../../assets/stadium.png";
+import { FaCalendar } from 'react-icons/fa6';
+import moment from 'moment';
 
 const LeftNavBar = () => {
   const [categories, setCategories] = useState([]);
@@ -18,7 +23,66 @@ const LeftNavBar = () => {
           categories.map(category =><button className='btn' key={category.category_id}>{category.category_name}</button>
           )
         }
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src={kid}
+              className='w-[200px]'
+              alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-xl font-semibold text-[#403F3F]">Bayern Slams Authorities Over Flight Delay to Club World Cup</h2>
+            
+            <div className="card-actions justify-between">
+              <p className="text-base font-medium text-[#403F3F]">Sports</p>
+              <div className='flex gap-1'>
+                <span> <FaCalendar></FaCalendar> </span>
+                <p className="text-base font-medium text-[#9F9F9F]">{moment().format('MMMM Do YYYY')}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src={office}
+              className='w-[200px]'
+              alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-xl font-semibold text-[#403F3F]">Bayern Slams Authorities Over Flight Delay to Club World Cup</h2>
+            
+            <div className="card-actions justify-between">
+              <p className="text-base font-medium text-[#403F3F]">Sports</p>
+              <div className='flex gap-1'>
+                <span> <FaCalendar></FaCalendar> </span>
+                <p className="text-base font-medium text-[#9F9F9F]">{moment().format('MMMM Do YYYY')}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src={stadium}
+              className='w-[200px]'
+              alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-xl font-semibold text-[#403F3F]">Bayern Slams Authorities Over Flight Delay to Club World Cup</h2>
+            
+            <div className="card-actions justify-between">
+              <p className="text-base font-medium text-[#403F3F]">Sports</p>
+              <div className='flex gap-1'>
+                <span> <FaCalendar></FaCalendar> </span>
+                <p className="text-base font-medium text-[#9F9F9F]">{moment().format('MMMM Do YYYY')}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+       
       </div>
+      
     </div>
   );
 };
